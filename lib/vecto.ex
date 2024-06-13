fc0772afb553b8393395980317882401a4ddf891
Oliver Kriska <oliver@ideax.sk>
2024-06-13 15:44:00 +0200

    Allow to use Pgvector struct instead only vector list

--- lib/vecto.ex
+++ lib/vecto.ex
@@ -33,7 +33,8 @@ defmodule Vecto do
   Uses Postgres vector search (pg_vector) to find documents that are semantically similar to the query embedding, ranked by similarity.
   """
   def semantic_search(schema, query_field, query_embedding, limit_by \\ 100, select_columns \\ [])
-      when is_atom(query_field) and is_list(query_embedding) do
+      when is_atom(query_field) and
+             (is_list(query_embedding) or is_struct(query_embedding, Pgvector)) do
     select_columns = build_select_columns(schema, select_columns)
 
     schema
@@ -59,7 +60,8 @@ defmodule Vecto do
     but is flexible enough to work with any table or column name
   """
   def hybrid_search(schema, query_embedding, query_string, opts \\ [])
-      when is_list(query_embedding) and is_binary(query_string) do
+      when (is_list(query_embedding) or is_struct(query_embedding, Pgvector)) and
+             is_binary(query_string) do
     opts = Keyword.merge(hybrid_search_default_opts(), opts)
     max_limit = opts[:limit] * 2
     select_columns = build_select_columns(schema, opts[:select_columns])
